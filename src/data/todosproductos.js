e6a97a9b19d98dd12a3e8260e17766641129a52e
Sebastian <ssalasg.mm@gmail.com>
2024-08-29 22:46:29 -0400

    Cree la pagina productos

---
+++ src/data/todosproductos.js
@@ -0,0 +1,120 @@
+export const TortaMoka = {
+    nombre: "MOKA",
+    descripcion: "Bizcocho de chocolate, relleno de manjar, crema moka (chantilly con café) y chips de chocolate.",
+    p10: 21000,
+    p15: 25900,
+    p20: 31300,
+    p30: 42000,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Normal"
+};
+
+export const TortaTresLeches = {
+    nombre: "TRES LECHES",
+    descripcion: "Bizcocho de vainilla bañado en leche evaporada, crema de leche y leche condensada, relleno de crema pastelera casera, manjar y cubierto de merengue suizo.",
+    p10: 22600,
+    p15: 28600,
+    p20: 37800,
+    p30: 47500,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Normal"
+};
+
+export const TortaTresLechesChocolate = {
+    nombre: "TRES LECHES CHOCOLATE",
+    descripcion: "Bizcocho de chocolate bañado en leche evaporada, crema de leche y leche condensada, relleno de manjar y cubierto de merengue suizo.",
+    p10: 27000,
+    p15: 31300,
+    p20: 41000,
+    p30: 50000,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Normal"
+};
+
+export const TortaChocolate = {
+    nombre: "CHOCOLATE",
+    descripcion: "Bizcocho de chocolate, relleno de crema de chocolate y ganache de chocolate.",
+    p10: 21000,
+    p15: 25900,
+    p20: 31300,
+    p30: 42000,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Normal"
+};
+
+export const TortaArandanoLimon = {
+    nombre: "ARÁNDANO LIMÓN",
+    descripcion: "Bizcocho de limón, relleno de crema de arándanos y arándanos enteros.",
+    p10: 21600,
+    p15: 28600,
+    p20: 39900,
+    p30: 50700,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Normal"
+};
+
+export const TortaRascaAmor = {
+    nombre: "AMOR (Hojarasca)",
+    descripcion: "Discos de hojarasca, rellenos de crema pastelera casera, manjar, mermelada de frutos rojos y crema chantilly.",
+    p10: 21600,
+    p15: 28600,
+    p20: 39900,
+    p30: 50700,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Normal"
+};
+
+export const TortaHMFrambuesa = {
+    nombre: "MERENGUE FRAMBUESA",
+    descripcion: "Discos de merengue, rellenos de crema chantilly, frambuesas y salsa de frambuesa.",
+    p10: 22600,
+    p15: 28600,
+    p20: 39900,
+    p30: 51800,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Helada"
+};
+
+export const TortaHMLucuma = {
+    nombre: "MERENGUE LÚCUMA",
+    descripcion: "Discos de merengue, rellenos de crema chantilly con puré de lúcuma y chips de chocolate.",
+    p10: 22600,
+    p15: 28600,
+    p20: 39900,
+    p30: 51800,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Helada"
+};
+
+export const TortaEZabahoria = {
+    nombre: "ZANAHORIA",
+    descripcion: "Bizcocho de zanahoria con nueces picadas, relleno de frosting de queso crema y manjar.",
+    p10: 27000,
+    p15: 34500,
+    p20: 44200,
+    p30: 59400,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Especial"
+};
+
+export const TortaEMixta = {
+    nombre: "MIXTA",
+    descripcion: "Combinación de bizcocho de chocolate, bizcocho de vainilla con amapola, discos de merengue y discos de hojarasca, rellenos de ganache de chocolate, manjar, crema chantilly y frambuesas.",
+    p10: 22600,
+    p15: 27000,
+    p20: 37200,
+    p30: 52000,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Especial"
+};
+
+export const TortaECastana = {
+    nombre: "CASTAÑA",
+    descripcion: "Bizcocho de vainilla, relleno de crema chantilly con puré de castaña, ganache de chocolate y un disco de merengue. Decorada con enrejado de chocolate.",
+    p10: 22600,
+    p15: 28600,
+    p20: 39900,
+    p30: 51800,
+    imagen: "productos/torta_amor.jpg",
+    tipo: "Especial"
+};
