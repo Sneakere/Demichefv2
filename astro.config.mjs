3d8a2bc462bd083f6ea564eb60ee4872cc36f5c3
Sebastian <ssalasg.mm@gmail.com>
2024-09-02 15:23:04 -0400

    xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Demichefv2.github.io/',
+  site: 'https://Sneakere.github.io',
   base: '/',
 });
