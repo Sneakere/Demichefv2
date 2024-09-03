1e753c6593152560a502d30be9e59f85bb94108c
Sebastian <ssalasg.mm@gmail.com>
2024-09-03 15:52:52 -0400

    rua

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+// const repoName = 'Demichefv2';
+// export default defineConfig({
+//   integrations: [tailwind()],
+//   site: `https://sneakere.github.io/${repoName}/`,
+//   base: '/Demichefv2/'
+// });
+
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-const repoName = 'Demichefv2';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   integrations: [tailwind()],
-  site: `https://sneakere.github.io/${repoName}/`,
-  base: '/Demichefv2/'
+  site: isProduction ? 'https://sneakere.github.io/Demichefv2/' : '',
+  base: isProduction ? '/Demichefv2/' : '/',
 });
