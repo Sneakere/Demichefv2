6f7c40c7931d937275a4adc6d6ad92f07064e5c0
Sebastian <ssalasg.mm@gmail.com>
2024-09-05 17:50:35 -0400

    xdxd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,3 @@
-// import { defineConfig } from 'astro/config';
-// import tailwind from '@astrojs/tailwind';
-
-// const isProduction = process.env.NODE_ENV === 'production';
-// const baseURL = isProduction ? '/Demichefv2/' : '/Demichefv2/';
-
-// export default defineConfig({
-//   integrations: [tailwind()],
-//   site: 'https://sneakere.github.io/Demichefv2/',
-//   base: baseURL
-// });
-
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
