77dfbda67411e9a2450cb5eccae3b0c6eb32c330
Sebastian <ssalasg.mm@gmail.com>
2024-09-07 13:04:59 -0400

    horarios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const baseURL = isProduction ? '/Demichefv2/' : '/Demichefv2/';
+const baseURL = '/Demichefv2/';
 
 export default defineConfig({
   integrations: [tailwind()],
